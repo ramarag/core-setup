dc73f91b4b047099fe2de8229be89411c50bb9e8
Rama Krishnan Raghupathy <ramarag@microsoft.com>
2017-04-05 18:40:09 -0700

    adding host messaging for missing runtimestore

--- src/corehost/cli/deps_entry.h
+++ src/corehost/cli/deps_entry.h
@@ -27,6 +27,7 @@ struct deps_entry_t
     pal::string_t library_hash;
     pal::string_t library_path;
     pal::string_t library_hash_path;
+    pal::string_t filter_project_list;
     asset_types asset_type;
     pal::string_t asset_name;
     pal::string_t relative_path;
